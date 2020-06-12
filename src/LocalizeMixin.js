1f6004121c984a65bf71fddc5cee5e85a1359b15
Ryan Goessl <Ryan.Goessl@D2L.com>
2020-06-12 14:29:36 -0400

    Fix lint

--- src/LocalizeMixin.js
+++ src/LocalizeMixin.js
@@ -1,32 +1,35 @@
 import { LocalizeMixin as CoreLocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin';
 
 const supportedLangs = [
-    'ar', 'de', 'en', 'es', 'fr', 'ja', 'ko', 'nl', 'pt', 'sv', 'tr', 'zh-tw', 'zh'
+	'ar', 'de', 'en', 'es', 'fr', 'ja', 'ko', 'nl', 'pt', 'sv', 'tr', 'zh-tw', 'zh'
 ];
 
 export const LocalizeMixin = (superclass) => class extends CoreLocalizeMixin(superclass) {
 
-    static async getLocalizeResources(langs) {
-        for await (const lang of langs) {
-            let translations;
+	static async getLocalizeResources(langs) {
+		let translations;
 
-            const langIndex = supportedLangs.indexOf(lang);
-            translations = await import(`../build/lang/${supportedLangs[langIndex]}.js`);
+		for await (const lang of langs) {
+			const langIndex = supportedLangs.indexOf(lang);
+			if (langIndex >= 0) {
+				const langFile = `../build/lang/${supportedLangs[langIndex]}.js`;
+				translations = await import(langFile);
 
-            if (translations && translations.val) {
-                return {
-                    language: supportedLangs[langIndex],
-                    resources: translations.val
-                };
-            }
-        }
+				if (translations && translations.val) {
+					return {
+						language: supportedLangs[langIndex],
+						resources: translations.val
+					};
+				}
+			}
+		}
 
-        // Default lang
-        translations = await import('../build/lang/en.js');
-        return {
-            language: 'en',
-            resources: translations.val
-        };
-    }
+		// Default lang
+		translations = await import('../build/lang/en.js');
+		return {
+			language: 'en',
+			resources: translations.val
+		};
+	}
 
 };
