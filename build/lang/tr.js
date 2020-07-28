bdfeaf9ef549f7745f51aa3ad47c75739197e5af
Ryan Goessl <Ryan.Goessl@D2L.com>
2020-07-28 17:23:42 -0400

    Add langterms and mock data

--- build/lang/tr.js
+++ build/lang/tr.js
@@ -1,3 +1,6 @@
 export const val = {
-	'headingTotalAssessments': 'Total Assessments: {num}'
+	'headingTotalAssessments': 'Total Assessments: {num}',
+	'indexedAttempt': 'Attempt {num}',
+	'miniTrendScreenReaderText': 'Assessed {numAssessed} {numAssessed, plural, =1 {time} other {times}}: {levelNames}',
+	'notAssessed': 'Not assessed'
 };
