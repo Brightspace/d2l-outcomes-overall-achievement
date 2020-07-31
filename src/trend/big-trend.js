02da542cabdec652cb6eef855068b721b256b321
Ryan Goessl <Ryan.Goessl@D2L.com>
2020-07-31 14:05:35 -0400

    Convert big-trend to LitElement

---
+++ src/trend/big-trend.js
@@ -0,0 +1,657 @@
+import { LitElement, html, css } from 'lit-element';
+import { LocalizeMixin } from '../LocalizeMixin';
+import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin';
+import { TrendMixin } from './TrendMixin';
+import '@brightspace-ui/core/components/colors/colors';
+import '@brightspace-ui/core/components/icons/icon';
+import '@brightspace-ui/core/components/tooltip/tooltip';
+import { formatDate } from '@brightspace-ui/intl/lib/dateTime.js';
+import { ResizeObserver } from 'd2l-resize-aware/resize-observer-module';
+
+const COMPONENT_HEIGHT = 120;
+const FOOTER_HEIGHT = 22;
+const GRID_THICKNESS = 1;
+const NOT_ASSESSED_HEIGHT = 4;
+const TOOLTIP_GAP = 8;
+const TOOLTIP_POINTER_SIZE = 8;
+const SCROLL_VIEWPORT_FRACTION = 0.5;
+
+class BigTrend extends TrendMixin(LocalizeMixin(RtlMixin(LitElement))) {
+
+	static get is() { return 'd2l-coa-big-trend'; }
+
+	static get properties() {
+		return {
+			instructor: { type: Boolean },
+			outcomeTerm: { type: String }
+		};
+	}
+
+	static get styles() {
+		return [
+			css`
+				:host {
+					--block-focus-height-increase: 4px;
+					--block-focus-width-increase: 110%;
+					--block-max-width: 54px;
+					--block-min-width: 24px;
+					--block-spacing: 9px;
+					--border-radius: 6px;
+					--container-height: ${COMPONENT_HEIGHT}px;
+					--footer-height: ${FOOTER_HEIGHT}px;
+					--grid-color: #d3d9e3;
+					--grid-label-color: #7C8695;
+					--grid-thickness: ${GRID_THICKNESS}px;
+					--label-font-size: 14px;
+					--label-margin-top: 0;
+					--max-tooltip-width: 210px;
+					--not-assessed-color: #d3d9e3;
+					--not-assessed-height: ${NOT_ASSESSED_HEIGHT}px;
+					--scroll-button-width: 50px;
+				}
+
+				#container {
+					position: relative;
+				}
+	
+				#grid {
+					float: left;
+					padding-top: var(--block-focus-height-increase);
+					position: relative;
+					width: 100%;
+				}
+	
+				.h-line {
+					background-color: var(--grid-color);
+					height: var(--grid-thickness);
+				}
+
+				#scroll-container {
+					height: calc(var(--container-height) + var(--block-focus-height-increase) + var(--footer-height));
+					left: 0px;
+					overflow-y: hidden;
+					position: absolute;
+					top: 0px;
+					width: 100%;
+				}
+
+				#scroll {
+					height: calc(var(--container-height) + var(--block-focus-height-increase) + var(--footer-height));
+					overflow-x: scroll;
+					overflow-y: hidden;
+					padding: 0px var(--block-spacing);
+					padding-bottom: 20px;
+					scroll-behavior: smooth;
+					width: calc(100% - 2 * var(--block-spacing));
+				}
+
+				.scroll-button {
+					align-items: center;
+					background: rgba(255, 255, 255, 0.5);
+					display: flex;
+					height: calc(var(--container-height) + var(--grid-thickness) + var(--footer-height));
+					position: absolute;
+					top: var(--block-focus-height-increase);
+					vertical-align: middle;
+					width: var(--scroll-button-width);
+				}
+
+				.scroll-button:hover {
+					cursor: pointer;
+					filter: brightness(120%);
+				}
+
+				#scroll-button-left {
+					justify-content: flex-start;
+				}
+
+				:host(:not([dir="rtl"])) #scroll-button-left,
+				:host([dir="rtl"]) #scroll-button-right {
+					background: linear-gradient(90deg, white, transparent);
+					left: 0px;
+				}
+
+				#scroll-button-right {
+					justify-content: flex-end;
+				}
+				
+				:host(:not([dir="rtl"])) #scroll-button-right,
+				:host([dir="rtl"]) #scroll-button-left {
+					background: linear-gradient(-90deg, white, transparent);
+					right: 0px;
+				}
+	
+				#data {
+					align-items: flex-end;
+					display: flex;
+					flex-direction: row;
+					height: calc(var(--container-height) + var(--block-focus-height-increase));
+				}
+
+				.grid-column {
+					display: flex;
+					flex-direction: column;
+					height: var(--container-height);
+					justify-content: flex-end;
+					max-width: var(--block-max-width);
+					min-width: var(--block-min-width);
+					padding: 0px var(--block-spacing);
+					padding-bottom: calc(var(--footer-height) + var(--block-spacing));
+					position: relative;
+					top: calc(var(--footer-height) + var(--block-spacing));
+					width: 100%;
+				}
+
+				.grid-column.section:not(:first-of-type) {
+					border-inline-start: var(--grid-thickness) solid var(--grid-color);
+				}
+	
+				.trend-group {
+					align-items: center;
+					display: flex;
+					flex-direction: column;
+					overflow: visible;
+				}
+
+				.grid-label { /* Must be different element type than .trend-block because of last-of-type selector */
+					box-sizing: border-box;
+					color: var(--grid-label-color);
+					display: inline-block;
+					font-size: var(--label-font-size);
+					height: 0px;
+					left: calc(var(--grid-thickness) * -1);
+					position: relative;
+					width: 100%;
+				}
+
+				.trend-block {
+					/* display: flex;
+					flex-direction: column;*/
+					flex-shrink: 0;
+					margin-bottom: var(--grid-thickness);
+					transition: all 0.3s ease-out;
+					width: 100%;
+				}
+
+				.not-assessed .trend-block {
+					background-color: var(--not-assessed-color);
+					height: var(--not-assessed-height);
+				}
+	
+				.trend-group .trend-block:first-of-type { /* Requires trend-blocks and not-assessed trend-blocks to be different element types */
+					border-top-left-radius: var(--border-radius);
+					border-top-right-radius: var(--border-radius);
+				}
+	
+				.trend-group .trend-block:last-of-type {
+					margin-bottom: 0px;
+				}
+
+				.trend-group:hover,
+				.trend-group:focus {
+					cursor: pointer;
+					outline: none;
+				}
+				
+				.trend-group:not(.not-assessed):hover .trend-block,
+				.trend-group:not(.not-assessed):focus .trend-block {
+					filter: brightness(120%);
+					box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
+				}
+	
+				.screen-reader {
+					height: 1px;
+					left: -99999px;
+					overflow: hidden;
+					position: absolute;
+					width: 1px;
+				}
+
+				d2l-tooltip {
+					max-width: var(--max-tooltip-width);
+					text-align: center;
+				}
+
+				.clear {
+					clear: both;
+				}
+
+				.hidden {
+					display: none !important;
+				}
+
+				table {
+					border-collapse: collapse;
+					border: 1px solid black;
+				}
+
+				table td,
+				table th {
+					border: 1px solid black;
+				}
+
+				.no-scale {
+					border: 1px solid var(--d2l-color-gypsum);
+					border-radius: 8px;
+					background-color: var(--d2l-color-regolith);
+					color: var(--d2l-color-ferrite);
+					padding: 15px;
+					box-sizing: border-box;
+					width: 100%;
+				}
+			`
+		];
+	}
+
+	constructor() {
+		super();
+		this.instructor = false;
+	}
+
+	firstUpdated() {
+		this.scrollContainer = this.renderRoot.getElementById('scroll');
+		this.scrollButtonLeft = this.renderRoot.getElementById('scroll-button-left');
+		this.scrollButtonRight = this.renderRoot.getElementById('scroll-button-right');
+
+		this.scrollContainer.addEventListener('scroll', this._onDataScrolled.bind(this));
+		this.scrollButtonLeft.addEventListener('click', this._onScrollButtonClicked.bind(this));
+		this.scrollButtonRight.addEventListener('click', this._onScrollButtonClicked.bind(this));
+
+		this.resizeObserver = new ResizeObserver(() => {
+			this._onDataScrolled();
+			this._scrollToEnd();
+		});
+
+		this.resizeObserver.observe(this.scrollContainer);
+	}
+
+	render() {
+		if (this._hasNoScale(this._trendData)) {
+			const langTerm = this.instructor ? 'noScaleInstructor' : 'noScaleStudent';
+			return html`<div class="no-scale">${this.localize(langTerm, 'outcome', this.outcomeTerm)}</div>`;
+		}
+
+		const rowHeight = this._getRowHeight(this._trendData);
+		const gridHorizontal = this._getGridHorizontal(this._trendData, rowHeight);
+		const trendItems = this._getTrendItems(this._trendData, rowHeight);
+
+		// Reset scroll after each re-render
+		setTimeout(() => {
+			this._onDataScrolled();
+			this._scrollToEnd();
+		}, 0);
+
+		return html`
+			<div id="container" aria-hidden="true">
+				<div id="grid">
+					${gridHorizontal.map(item => html`<div class="h-line" style="margin-bottom: ${item.size}px;"></div>`)}
+				</div>
+				<div id="scroll-container">
+					<div id="scroll">
+						<div id="data">
+							${trendItems.map(this._renderTrendItem, this)}
+						</div>
+					</div>
+				</div>
+				<div id="scroll-button-left" class="scroll-button hidden">
+					<d2l-icon icon="d2l-tier1:chevron-left"></d2l-icon>
+				</div>
+				<div id="scroll-button-right" class="scroll-button hidden">
+					<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>
+				</div>
+				<div class="clear"></div>
+				${trendItems.map((item, i) => this._renderTrendItemTooltip(item, i, rowHeight))}
+			</div>
+			<div class="screen-reader">
+				${this._renderScreenReaderText(trendItems)}
+			</div>
+		`;
+	}
+
+	_checkTrendData(trendData) {
+		if (!trendData || !trendData.groups) {
+			return false;
+		}
+
+		const trendGroups = trendData.groups;
+		return trendGroups.length > 0 && trendGroups[0].attempts.length > 0;
+	}
+
+	_getAttemptGroupLabel(attempts) {
+		return this._getAttemptGroupNames(attempts).join(', ');
+	}
+
+	_getAttemptGroupNames(attempts) {
+		return attempts.map(attempt => attempt.name || this.localize('indexedAttempt', { num: attempt.attemptIndex }));
+	}
+
+	_getAttemptGroupScreenReaderText(attempts) {
+		attempts = this._getAttemptGroupNames(attempts);
+
+		const attemptNames = attempts.length > 1 ? attempts.slice(0, -1).join(', ') : attempts[0];
+		const lastAttemptName = attempts.slice(-1);
+
+		return this.localize(
+			'bigTrendAttemptsScreenReaderString',
+			'numAttempts', attempts.length,
+			'attemptNames', attemptNames,
+			'lastAttemptName', lastAttemptName
+		);
+	}
+
+	_getColumnClasses(group) {
+		const classes = [
+			'grid-column'
+		];
+
+		if (group.label) {
+			classes.push('section');
+		}
+
+		return classes.join(' ');
+	}
+
+	_getGridHorizontal(trendData, rowHeight) {
+		if (!trendData || !trendData.levels) {
+			return [];
+		}
+
+		const levels = trendData.levels;
+		const maxLevel = this._getMaxLevelScore(levels);
+		const gridHeight = rowHeight - GRID_THICKNESS;
+
+		const gridData = Array.apply(null, { length: maxLevel + 1 }).map((v, i) => {
+			return {
+				size: (i === maxLevel
+					? FOOTER_HEIGHT
+					: gridHeight
+				)
+			};
+		});
+		return gridData;
+	}
+
+	_getGroupClasses(group) {
+		const classes = [
+			'trend-group'
+		];
+
+		if (!this._groupHasBlocks(group)) {
+			classes.push('not-assessed');
+		}
+
+		return classes.join(' ');
+	}
+
+	_getGroupLabel(group) {
+		return formatDate(group.date, { format: 'MMM' });
+	}
+
+	_getMaxLevelScore(levels) {
+		return Math.max.apply(null, Object.keys(levels).map(levelId => levels[levelId].score));
+	}
+
+	_getRowHeight(trendData) {
+		if (!trendData || !trendData.levels) {
+			return null;
+		}
+
+		const maxLevel = this._getMaxLevelScore(trendData.levels);
+		return COMPONENT_HEIGHT / maxLevel;
+	}
+
+	_getScreenReaderTableHeadings() {
+		return [
+			this.localize('headingDate'),
+			this.localize('headingEvidence'),
+			this.localize('headingLoa')
+		];
+	}
+
+	_getTooltipOffset(group, rowHeight) {
+		let offset = TOOLTIP_POINTER_SIZE + TOOLTIP_GAP;
+
+		if (!this._groupHasBlocks(group)) {
+			offset -= rowHeight - GRID_THICKNESS - NOT_ASSESSED_HEIGHT;
+		}
+
+		return offset;
+	}
+
+	_getTrendItems(trendData, rowHeight) {
+		if (!trendData || !trendData.levels || !trendData.groups) {
+			return [];
+		}
+
+		const levels = trendData.levels;
+		const trendGroups = trendData.groups;
+		const trendItems = [];
+		const maxLevel = this._getMaxLevelScore(levels);
+		const gridHeight = rowHeight - GRID_THICKNESS;
+		let lastGroupId = null;
+
+		trendGroups.forEach(group => {
+			const blocks = [];
+
+			const groupAttempts = group.attempts;
+			const groupDate = formatDate(group.date, { format: 'MMMM d, yyyy' });
+			const groupId = formatDate(group.date, { format: 'yyyy-MM' });
+			const groupLabel = this._getGroupLabel(group);
+			const groupName = (!group.name || group.name.trim() === '') ? this.localize('untitled') : group.name;
+
+			const groupItem = {
+				date: groupDate,
+				gridHeight: gridHeight,
+				name: groupName
+			};
+
+			// Create vertical grid lines
+			if (groupId !== lastGroupId) {
+				groupItem.label = groupLabel;
+			}
+
+			lastGroupId = groupId;
+
+			// Compute levels achieved
+			const groupLevels = groupAttempts
+				.filter((val, index, self) => self.indexOf(val) === index)
+				.sort((left, right) => levels[left.levelId].score - levels[right.levelId].score);
+
+			// Add trend blocks to group
+			let prevScore = 0;
+			for (const attempt of groupLevels) {
+				const levelId = attempt.levelId;
+				const color = levels[levelId].color;
+				const height = COMPONENT_HEIGHT / maxLevel * (levels[levelId].score - prevScore) - GRID_THICKNESS;
+				prevScore = levels[levelId].score;
+
+				if (height <= 0) {
+					continue;
+				}
+
+				blocks.push({
+					color,
+					height
+				});
+			}
+
+			groupItem.blocks = blocks.reverse();
+			// Group attempt labels
+			const attemptLabels = [];
+			let attemptCounter = 1;
+			groupAttempts.forEach(attempt => {
+				const levelId = attempt.levelId;
+				let label = {
+					id: levelId,
+					name: levels[levelId].name,
+					attempts: [ {
+						attemptIndex: attemptCounter,
+						name: attempt.name
+					} ]
+				};
+				const prevAttempt = attemptLabels.pop();
+
+				if (prevAttempt && prevAttempt.id === levelId) {
+					label = prevAttempt;
+					label.attempts.push({
+						attemptIndex: attemptCounter,
+						name: attempt.name
+					});
+				} else if (prevAttempt) {
+					attemptLabels.push(prevAttempt);
+				}
+
+				attemptLabels.push(label);
+				attemptCounter++;
+			});
+
+			groupItem.attempts = attemptLabels;
+
+			trendItems.push(groupItem);
+		}, this);
+
+		return trendItems;
+	}
+
+	_getUniqueGroupId(groupIndex) {
+		return `group${groupIndex}`;
+	}
+
+	_groupHasBlocks(group) {
+		return group.blocks.length > 0;
+	}
+
+	_hasMultipleAttempts(group) {
+		return group.attempts.length > 0 && (group.attempts.length > 1 || group.attempts[0].attempts.length > 1);
+	}
+
+	_hasNoScale(trendData) {
+		return trendData && trendData.levels && Object.keys(trendData.levels).length === 0;
+	}
+
+	_isRtl() {
+		return this._dir === 'rtl';
+	}
+
+	_onDataScrolled() {
+		const scrollMax = this.scrollContainer.scrollLeftMax
+			|| (this.scrollContainer.scrollWidth - this.scrollContainer.offsetWidth);
+
+		if (this.scrollContainer.scrollLeft === 0 && !this.scrollButtonLeft.classList.contains('hidden')) {
+			this.scrollButtonLeft.classList.add('hidden');
+		} else if (this.scrollContainer.scrollLeft !== 0 && this.scrollButtonLeft.classList.contains('hidden')) {
+			this.scrollButtonLeft.classList.remove('hidden');
+		}
+
+		if (Math.abs(this.scrollContainer.scrollLeft) === scrollMax && !this.scrollButtonRight.classList.contains('hidden')) {
+			this.scrollButtonRight.classList.add('hidden');
+		} else if (Math.abs(this.scrollContainer.scrollLeft !== scrollMax) && this.scrollButtonRight.classList.contains('hidden')) {
+			this.scrollButtonRight.classList.remove('hidden');
+		}
+	}
+
+	_onScrollButtonClicked(e) {
+		const scrollButton = e.currentTarget;
+		let scrollAmount = SCROLL_VIEWPORT_FRACTION * this.scrollContainer.offsetWidth;
+
+		if (scrollButton === this.scrollButtonLeft) {
+			scrollAmount *= -1;
+		}
+
+		if (this._isRtl()) {
+			scrollAmount *= -1;
+		}
+
+		this.scrollContainer.scrollLeft += scrollAmount;
+	}
+
+	_renderScreenReaderText(trendItems) {
+		const hasData = this._checkTrendData(this._trendData);
+
+		if (!hasData) {
+			return this.localize('notAssessed');
+		}
+
+		const headings = this._getScreenReaderTableHeadings();
+
+		const renderedItems = trendItems.map(trendItem => {
+			const attempts = trendItem.attempts.map(attemptGroup => {
+				return html`
+					<div>
+						${this._hasMultipleAttempts(trendItem) ? this._getAttemptGroupScreenReaderText(attemptGroup.attempts) : null}
+						${attemptGroup.name}
+					</div>
+				`;
+			});
+
+			return html`
+				<tr>
+					<td>${trendItem.date}</td>
+					<td>${trendItem.name}</td>
+					<td>
+						${trendItem.blocks.length > 0 ? attempts : html`<div>${this.localize('notAssessed')}</div>`}
+					</td>
+				</tr>
+			`;
+		});
+
+		return html`
+			<table>
+				<thead>
+					<tr>
+						${headings.map(h => html`<th>${h}</th>`)}
+					</tr>
+				</thead>
+				<tbody>
+					${renderedItems}
+				</tbody>
+			</table>
+		`;
+	}
+
+	_renderTrendItem(trendItem, index) {
+		const blocks = trendItem.blocks.length > 0
+			? trendItem.blocks.map(block => html`<div class="trend-block" style="height: ${block.height}px; background-color: ${block.color};"></div>`)
+			: html`<span class="trend-block" style="margin-top: calc(${trendItem.gridHeight}px - var(--not-assessed-height));"></span>`;
+
+		const label = trendItem.label
+			? html`<span class="grid-label">${trendItem.label}</span>`
+			: null;
+
+		return html`
+			<div class=${this._getColumnClasses(trendItem)}>
+				<div id=${this._getUniqueGroupId(index)} class=${this._getGroupClasses(trendItem)} tabindex="0">
+					${blocks}
+				</div>
+				${label}
+			</div>
+		`;
+	}
+
+	_renderTrendItemTooltip(trendItem, index, rowHeight) {
+		const attempts = trendItem.attempts.map(attemptGroup => {
+			return html`
+				<div>
+					${this._hasMultipleAttempts(trendItem) ? html`<b>${this._getAttemptGroupLabel(attemptGroup.attempts)}</b>:` : null} 
+					${attemptGroup.name}
+				</div>
+			`;
+		});
+
+		return html`
+			<d2l-tooltip for=${this._getUniqueGroupId(index)} position="top" offset=${this._getTooltipOffset(trendItem, rowHeight)}>
+				<div><b>${trendItem.name}</b></div>
+				${trendItem.blocks.length > 0 ? attempts : html`<div>${this.localize('notAssessed')}</div>`}
+			</d2l-tooltip>
+		`;
+	}
+
+	_scrollToEnd() {
+		const scrollMax = this.scrollContainer.scrollLeftMax
+			|| (this.scrollContainer.scrollWidth - this.scrollContainer.offsetWidth);
+
+		this.scrollContainer.scrollLeft = scrollMax * (this._isRtl() ? -1 : 1);
+	}
+}
+
+customElements.define(BigTrend.is, BigTrend);
