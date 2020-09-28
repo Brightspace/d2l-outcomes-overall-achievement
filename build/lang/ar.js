173ef8d5675974df42b6e9eeb9352ee130247599
Callum <mitccall@gmail.com>
2020-09-28 14:54:55 -0400

    lang

--- build/lang/ar.js
+++ build/lang/ar.js
@@ -12,6 +12,7 @@ export const val = {
 	'noScaleInstructor': 'To view learner progress towards achieving this {outcome, select, competencies {competency} expectations {expectation} objectives {objective} outcomes {outcome} standards {standard} other {standard}}, create and select an achievement scale.',
 	'noScaleStudent': 'No scale has been set to measure achievement of {outcome, select, competencies {competencies} expectations {expectations} objectives {objectives} outcomes {outcomes} standards {standards} other {standards}}.',
 	'notAssessed': 'Not assessed',
+	'notEvaluated': 'Not evaluated',
 	'tooltipUserOutcomeAssessments': '{numAssessed} out of {numTotal} activities assessed',
 	'untitled': 'Untitled'
 };
