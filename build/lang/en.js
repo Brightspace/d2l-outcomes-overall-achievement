545a30f6103e10814ebf6824d331dea2d1ba9f5c
Callum <mitccall@gmail.com>
2020-10-05 12:42:44 -0400

    langterms

--- build/lang/en.js
+++ build/lang/en.js
@@ -1,6 +1,9 @@
 export const val = {
+	'ascending': 'Ascending',
 	'bigTrendAttemptsScreenReaderString': '{numAttempts, plural, =1 {Attempt {attemptNames}} other {Attempts {attemptNames} and {lastAttemptName}}}',
+	'descending': 'Descending',
 	'firstName': 'First name',
+	'goToUserAchievementSummaryPage': 'Go to {username}\'s achievement summary page',
 	'headingDate': 'Date',
 	'headingEvidence': 'Evidence Name',
 	'headingLoa': 'Level of Achievement',
@@ -8,11 +11,17 @@ export const val = {
 	'indexedAttempt': 'Attempt {num}',
 	'labelOverallAchievement': 'Overall Achievement',
 	'lastName': 'Last name',
+	'learnerSortButtonDescription': 'Select to sort table rows by user {newSortKey} {newSortDirection}. Current sort order is by {currentSortKey} {currentSortDirection}.',
+	'manualOverride': 'manual override',
 	'miniTrendScreenReaderText': 'Assessed {numAssessed} {numAssessed, plural, =1 {time} other {times}}: {levelNames}',
 	'noScaleStudent': 'No scale has been set to measure achievement of {outcome, select, competencies {competencies} expectations {expectations} objectives {objectives} outcomes {outcomes} standards {standards} other {standards}}.',
 	'noScaleInstructor': 'To view learner progress towards achieving this {outcome, select, competencies {competency} expectations {expectation} objectives {objective} outcomes {outcome} standards {standard} other {standard}}, create and select an achievement scale.',
 	'notAssessed': 'Not assessed',
 	'notEvaluated': 'Not evaluated',
-	'tooltipUserOutcomeAssessments': '{numAssessed} out of {numTotal} activities assessed',
+	'notPublished': 'not published',
+	'outOfDate': 'out-of-date',
+	'pressToOpenTheEvaluationPage': 'Press to open the evaluation page',
+	'published': 'published',
+	'tooltipUserOutcomeAssessments': '{numAssessed} out of {numTotal} activities are assessed',
 	'untitled': 'Untitled'
 };
