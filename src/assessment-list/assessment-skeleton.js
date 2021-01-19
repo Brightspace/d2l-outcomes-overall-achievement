1943c9433fa6ece396fa5067d5b8ac592a46a0fc
Rebecca Zhou <rebecca.zhou.ca@gmail.com>
2021-01-19 17:53:11 -0500

    assessment-skeleton replaced with the reusable skeleton

--- src/assessment-list/assessment-skeleton.js
+++ src/assessment-list/assessment-skeleton.js
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from 'lit-element';
 import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit';
 import { LocalizeMixin } from '../LocalizeMixin';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
 import '@brightspace-ui/core/components/colors/colors';
 
-export class AssessmentSkeleton extends EntityMixinLit(LocalizeMixin(LitElement)) {
+export class AssessmentSkeleton extends SkeletonMixin(EntityMixinLit(LocalizeMixin(LitElement))) {
 
 	static get is() {
 		return 'd2l-coa-assessment-skeleton';
@@ -14,124 +15,121 @@ export class AssessmentSkeleton extends EntityMixinLit(LocalizeMixin(LitElement)
 	}
 
 	static get styles() {
-		return css`
-			@keyframes body-pulse {
-				from { background-color: var(--d2l-color-sylvite); }
-				to { background-color: var(--d2l-color-regolith); }
-			}
-			
-			@keyframes border-pulse {
-				from { border-color: var(--d2l-color-gypsum); }
-				to { border-color: var(--d2l-color-sylvite); }
-			}
-			
-			.icon, .card-right, .card-top, .mini-icon, .line {
-				animation-name: body-pulse;
-				animation-duration: 0.9s;
-				animation-direction: alternate;
-				animation-iteration-count: infinite;
-			}
-			
-			.skeleton {
-				display: flex;
-				height: 124px;
-			}
-			
-			.timeline {
-				flex-grow: 0;
-				flex-shrink: 0;
-			}
-			
-			.card {
-				display: flex;
-				flex-grow: 1;
-				flex-shrink: 1;
-				height: 100%;
-				border-radius: 4px;
-				padding: 17px 23px 20px 23px;
-				box-sizing: border-box;
-				border: 1px solid;
-				animation-name: border-pulse;
-				animation-duration: 0.9s;
-				animation-direction: alternate;
-				animation-iteration-count: infinite;
-			}
-			
-			.icon {
-				margin: 18px;
-				width: 24px;
-				height: 24px;
-				border-radius: 4px;
-			}
-			
-			.card-right {
-				flex-grow: 0;
-				flex-shrink: 0;
-				border-radius: 50%;
-				width: 24px;
-				height: 24px;
-			}
-			
-			.card-left {
-				margin-top: 7px;
-				margin-right: 37px;
-				flex-grow: 1;
-				flex-shrink: 1;
-			}
-			
-			.card-top {
-				margin-bottom: 26px;
-				width: 38%;
-				height: 14px;
-				border-radius: 4px;
-			}
-			
-			.card-bottom {
-				display: flex;
-			}
-			
-			.mini-icon {
-				width: 15px;
-				height: 15px;
-				flex-grow: 0;
-				flex-shrink: 0;
-				border-radius: 4px;
-			}
-			
-			.lines {
-				margin-left: 15px;
-				flex-grow: 1;
-				flex-shrink: 1;
-			}
-			
-			.line {
-				width: 100%;
-				height: 10px;
-				margin-top: 4px;
-				margin-bottom: 9px;
-				border-radius: 4px;
-			}
-		`;
+		return [
+			css`			
+				@keyframes border-pulse {
+					from { border-color: var(--d2l-color-gypsum); }
+					to { border-color: var(--d2l-color-sylvite); }
+				}
+				
+				.skeleton-container {
+					display: flex;
+					height: 124px;
+					margin-bottom: 18px;
+				}
+				
+				.timeline {
+					flex-grow: 0;
+					flex-shrink: 0;
+				}
+				
+				.card {
+					display: flex;
+					flex-grow: 1;
+					flex-shrink: 1;
+					height: 100%;
+					border-radius: 4px;
+					padding: 17px 23px 20px 23px;
+					box-sizing: border-box;
+					border: 1px solid;
+					animation-name: border-pulse;
+					animation-duration: 0.9s;
+					animation-direction: alternate;
+					animation-iteration-count: infinite;
+				}
+				
+				.icon {
+					margin: 18px;
+					width: 24px;
+					height: 24px;
+					border-radius: 4px;
+				}
+				
+				.card-right {
+					flex-grow: 0;
+					flex-shrink: 0;
+					border-radius: 50%;
+					width: 24px;
+					height: 24px;
+				}
+				
+				.card-left {
+					margin-top: 7px;
+					margin-right: 37px;
+					flex-grow: 1;
+					flex-shrink: 1;
+				}
+				
+				.card-top {
+					margin-bottom: 26px;
+					width: 38%;
+					height: 14px;
+					border-radius: 4px;
+				}
+				
+				.card-bottom {
+					display: flex;
+				}
+				
+				.mini-icon {
+					width: 15px;
+					height: 15px;
+					flex-grow: 0;
+					flex-shrink: 0;
+					border-radius: 4px;
+				}
+				
+				.lines {
+					margin-left: 15px;
+					flex-grow: 1;
+					flex-shrink: 1;
+				}
+				
+				.line {
+					width: 100%;
+					height: 10px;
+					margin-top: 4px;
+					margin-bottom: 9px;
+					border-radius: 4px;
+				}
+			`,
+			super.styles
+		];
+	}
+
+	constructor() {
+		super();
+		this.skeleton = true;
 	}
 
 	render() {
 		return html`
-			<div class="skeleton">
+			<div class="skeleton-container">
 				<div class="timeline">
-					<div class="icon"></div>
+					<div class="icon d2l-skeletize"></div>
 				</div>
 				<div class="card">
 					<div class="card-left">
-						<div class="card-top"></div>
+						<div class="card-top d2l-skeletize"></div>
 						<div class="card-bottom">
-							<div class="mini-icon"></div>
+							<div class="mini-icon d2l-skeletize"></div>
 							<div class="lines">
-								<div class="line"></div>
-								<div class="line"></div>
+								<div class="line d2l-skeletize"></div>
+								<div class="line d2l-skeletize"></div>
 							</div>
 						</div>
 					</div>
-					<div class="card-right"></div>
+					<div class="card-right d2l-skeletize"></div>
 				</div>
 			</div>
 		`;
