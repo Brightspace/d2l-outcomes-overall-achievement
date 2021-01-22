2acf02c2086b1f78cf5a16465134b647569cb3b5
Callum <mitccall@gmail.com>
2021-01-22 17:16:34 -0500

    Add basic panel layout skeleton

--- src/primary-panel/primary-panel.js
+++ src/primary-panel/primary-panel.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit';
 import { LocalizeMixin } from '../LocalizeMixin';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
 import { heading3Styles } from '@brightspace-ui/core/components/typography/styles';
 import '@brightspace-ui/core/components/button/button-icon';
 import '../outcome-text-display/outcome-text-display';
@@ -11,7 +12,7 @@ import '../trend/big-trend';
 import { UserProgressOutcomeEntity } from '../entities/UserProgressOutcomeEntity';
 import { Consts } from '../consts';
 
-class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
+class PrimaryPanel extends EntityMixinLit(SkeletonMixin(LocalizeMixin(LitElement))) {
 	static get is() {
 		return 'd2l-coa-primary-panel';
 	}
@@ -53,8 +54,79 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 					flex-grow: 0;
 					margin-left: 16px;
 				}
+
+				
+				.outcome-text-line1-skeleton {
+					display: block;
+					width: 100%;
+					height: 1.1rem;
+					margin-bottom: 0.5rem;
+				}
+
+				.outcome-text-line2-skeleton {
+					display: block;
+					width: 85%;
+					height: 1.1rem;
+					margin-bottom: 1.2rem;
+				}
+
+				.outcome-notation-skeleton {
+					display: block;
+					width: 6rem;
+					height: 0.8rem;
+					margin-bottom: 1.8rem;
+				}
+
+				.trend-title-skeleton {
+					display: block;
+					width: 6rem;
+					height: 1.1rem;
+					margin-bottom: 1.8rem;
+				}
+
+				.trend-graph-skeleton {
+					display: block;
+					width: 100%;
+					height: 6.3rem;
+					margin-bottom: 2.4rem;
+				}
+
+				.overall-achievement-tile-skeleton {
+					display: block;
+					width: 100%;
+					height: 5.1rem;
+					margin-bottom: 1.8rem;
+				}
+
+				.evidence-title-skeleton {
+					display: block;
+					width: 6rem;
+					height: 1.1rem;
+					margin-bottom: 1.8rem;
+				}
+
+				.assessment-skeleton-container {
+					display: flex;
+				}
+
+				.assessment-date-skeleton {
+					display: inline-block;
+					width: 1.5rem;
+					height: 1.5rem;
+					margin-left: 0.6rem;
+				}
+
+				.assessment-tile-skeleton {
+					display: inline-block;
+					flex-grow: 1;
+					height: 5.1rem;
+					margin-left: 0.6rem;
+					margin-bottom: 0.9rem;
+				}
+
 			`,
-			heading3Styles
+			heading3Styles,
+			super.styles
 		];
 	}
 
@@ -69,9 +141,15 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 		this._outcomeActivitiesHref = '';
 		this._checkpointHref = '';
 		this._checkpointPublished = false;
+		this.skeleton = true;
 	}
 
 	render() {
+
+		if(this.skeleton) {
+			return this._renderPanelSkeleton();
+		}
+
 		const closeButton = this.showClose ? html`
 			<d2l-button-icon
 				class="close-button"
@@ -92,8 +170,9 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 			<div id="header">
 				<d2l-coa-outcome-text-display 
 					href="${this._outcomeHref}" 
-					.token="${this.token}">
-				</d2l-coa-outcome-text-display>
+					.token="${this.token}"
+					?skeleton=${this.skeleton}
+				/>
 				${closeButton}
 			</div>
 
@@ -121,8 +200,8 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 
 			<d2l-coa-assessment-list
 				href="${this.href}"
-				.token="${this.token}">
-			</d2l-coa-assessment-list>
+				.token="${this.token}"
+			/>
 		`;
 	}
 
@@ -146,6 +225,9 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 			let checkpointPublished;
 			const outcomeHref = entity.getOutcomeHref();
 			const outcomeActivitiesHref = entity.getOutcomeActivitiesHref();
+
+			
+
 			entity.onOutcomeActivitiesChanged(outcomeActivities => {
 				if (!outcomeActivities) {
 					return;
@@ -162,14 +244,40 @@ class PrimaryPanel extends EntityMixinLit(LocalizeMixin(LitElement)) {
 					}
 				});
 			});
-
+			
 			entity.subEntitiesLoaded().then(() => {
 				this._outcomeHref = outcomeHref;
 				this._outcomeActivitiesHref = outcomeActivitiesHref;
 				this._checkpointHref = checkpointHref;
 				this._checkpointPublished = checkpointPublished;
+				this.skeleton = false;
 			});
 		}
+	}
+
+	_renderAssessmentPlaceholderSkeleton() {
+		return html`
+			<div class="assessment-skeleton-container">
+				<span class="assessment-date-skeleton d2l-skeletize"></span>
+				<span class="assessment-tile-skeleton d2l-skeletize"></span>
+			</div>
+		`
+	}
+
+	_renderPanelSkeleton() {
+		console.log("Skeleton!");
+		return html`
+			<div class="outcome-text-line1-skeleton d2l-skeletize"></div>
+			<div class="outcome-text-line2-skeleton d2l-skeletize"></div>
+			<div class="outcome-notation-skeleton d2l-skeletize"></div>
+			<div class="trend-title-skeleton d2l-skeletize"></div>
+			<div class="trend-graph-skeleton d2l-skeletize"></div>
+			<div class="overall-achievement-tile-skeleton d2l-skeletize"></div>
+			<div class="evidence-title-skeleton d2l-skeletize"></div>
+			${this._renderAssessmentPlaceholderSkeleton()}
+			${this._renderAssessmentPlaceholderSkeleton()}
+			${this._renderAssessmentPlaceholderSkeleton()}
+		`;
 	}
 }
 
