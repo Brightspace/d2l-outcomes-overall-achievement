62992226be862242d378b0125706fd9bd42d1305
Devon Powell <Devon.Powell@D2L.com>
2021-02-26 14:53:34 -0500

    Update dev dependencies
    This required fixing linting issues. No code was changed only moved.

--- src/entities/UserActivityUsageEntity.js
+++ src/entities/UserActivityUsageEntity.js
@@ -1,6 +1,14 @@
 import { Entity } from 'siren-sdk/src/es6/Entity';
 import { SelflessEntity } from 'siren-sdk/src/es6/SelflessEntity';
 
+class ActivityNameEntity extends SelflessEntity {
+	static get class() { return 'user-activity-name'; }
+
+	getShortText() {
+		return this._entity && this._entity.properties && this._entity.properties.shortText;
+	}
+}
+
 export class UserActivityUsageEntity extends Entity {
 	static get class() { return 'activity'; }
 	static get links() {
@@ -24,13 +32,5 @@ export class UserActivityUsageEntity extends Entity {
 
 		const nameEntity = this._entity.getSubEntityByClass(ActivityNameEntity.class);
 		return nameEntity && new ActivityNameEntity(this, nameEntity);
-	}
-}
-
-class ActivityNameEntity extends SelflessEntity {
-	static get class() { return 'user-activity-name'; }
-
-	getShortText() {
-		return this._entity && this._entity.properties && this._entity.properties.shortText;
 	}
 }
