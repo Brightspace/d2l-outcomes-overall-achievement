2df7a1179d57037b5b19b28051e2a13460978603
Ryan Goessl <Ryan.Goessl@D2L.com>
2021-03-17 14:34:03 -0400

    Add identifier to Outcome Entity
    Replicates functionality from legacy Polymer behaviour
    https://github.com/Brightspace/d2l-activity-alignments/blob/master/d2l-outcome-parser-behavior.js#L32

--- src/entities/OutcomeEntity.js
+++ src/entities/OutcomeEntity.js
@@ -1,5 +1,9 @@
 import { Entity } from 'siren-sdk/src/es6/Entity';
 
+const _trim = function(str) {
+	return (str && str.trim) ? str.trim() : null;
+};
+
 export class OutcomeEntity extends Entity {
 	static get class() { return 'outcome'; }
 
@@ -9,6 +13,29 @@ export class OutcomeEntity extends Entity {
 
 	getDescription() {
 		return this._entity && this._entity.properties && this._entity.properties.description;
+	}
+
+	getIdentifier() {
+		if (!this._entity || !this._entity.properties) {
+			return;
+		}
+
+		const props = this._entity.properties;
+
+		const notation = _trim(this.getNotation()) || _trim(this.getAltNotation());
+		const primarySubject = props.subjects && _trim(props.subjects.find(s => _trim(s)));
+
+		const outcomeInfo = [
+			primarySubject,
+			_trim(props.label),
+			_trim(props.listId)
+		].filter(id => id).join(' ');
+
+		if (outcomeInfo) {
+			return notation ? `${notation} - ${outcomeInfo}` : outcomeInfo;
+		}
+
+		return notation || '';
 	}
 
 	getNotation() {
