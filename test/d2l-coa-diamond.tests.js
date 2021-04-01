9d6d1a3290b153651aceb6aa4e1c44c36e32e66b
Devon Powell <Devon.Powell@D2L.com>
2021-03-31 21:31:14 -0400

    Explicitly add deepmerge

--- test/d2l-coa-diamond.tests.js
+++ test/d2l-coa-diamond.tests.js
@@ -5,9 +5,7 @@ describe('<d2l-coa-diamond>', () => {
 	let element;
 
 	beforeEach(async() => {
-		element = await fixture(html`
-			<d2l-coa-diamond></d2l-coa-diamond>
-		`);
+		element = await fixture(html`<d2l-coa-diamond></d2l-coa-diamond>`);
 	});
 
 	describe('smoke test', () => {
